09ac61cc79585bbeb668bb27c9d0865c3acab542
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-10-21 12:17:08 +0100

    Added guard for existing projects Updated tests Updated documentation
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- pkg/scaffold/scaffold_test.go
+++ pkg/scaffold/scaffold_test.go
@@ -2,36 +2,109 @@ package scaffold
 
 import (
 	"os"
-	"path/filepath"
 	"testing"
 
+	"github.com/amido/stacks-cli/internal/models"
 	"github.com/amido/stacks-cli/pkg/config"
-
-	"github.com/stretchr/testify/assert"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func TestSetProjectPath(t *testing.T) {
+func setupScaffoldTestCase(t *testing.T) (func(t *testing.T), string) {
+	// create a temporary directory
+	tempDir := t.TempDir()
 
-	// create a configuration object representing the config that might
-	// be injected
-	project_name := "test_project"
+	deferFunc := func(t *testing.T) {
+		err := os.RemoveAll(tempDir)
+		if err != nil {
+			t.Logf("[ERROR] Unable to remove dir: %v", err)
+		}
+	}
 
+	return deferFunc, tempDir
+}
+
+func TestAnalyseMissing(t *testing.T) {
+
+	// create test tables
+	tables := []struct {
+		missing []models.Command
+		test    string
+		msg     string
+	}{
+		{
+			[]models.Command{},
+			"",
+			"There should be no error message as there are no missing comamnds",
+		},
+		{
+			[]models.Command{
+				{
+					Binary:    "",
+					Framework: "dotnet",
+				},
+			},
+			`Some of the commands required by the specified frameworks do not exist on your machine or the framework has been specified incorrectly.
+
+Framework 'dotnet' may have been misspelled because the command for this framework cannot be determined`,
+			"An error message should be returned as there is 1 missing command",
+		},
+	}
+
+	// create the necessary objects
 	cfg := config.Config{}
-	// config.Input.Project[0].Name = project_name
-
-	project := make([]config.Project, 1)
-	project[0].Name = project_name
-	cfg.Input.Project = project
-
-	cfg.Input.Directory.WorkingDir = filepath.Join(os.TempDir())
-
 	logger := log.New()
+	scaffold := New(&cfg, logger)
 
-	s := New(&cfg, logger)
+	// iterate around the test tables and perform the tests
+	for _, table := range tables {
+		res := scaffold.analyseMissing(table.missing)
 
-	actual := filepath.Join(os.TempDir(), project_name)
+		if res != table.test {
+			t.Error(table.msg)
+		}
+	}
 
-	assert.Equal(t, actual, s.setProjectPath(project_name))
+}
+
+func TestConfigurePipeline(t *testing.T) {
+
+	cleanup, tempDir := setupScaffoldTestCase(t)
+	defer cleanup(t)
+
+	// create the test tables for the different configurations
+	tables := []struct {
+		cfg  config.Config
+		test bool
+	}{
+		{
+			config.Config{
+				Input: config.InputConfig{
+					Pipeline: "azdo",
+					Options: config.Options{
+						DryRun: true,
+					},
+					Project: []config.Project{
+						{
+							Directory: config.Directory{
+								WorkingDir: tempDir,
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+	}
+
+	// iterate around the tables
+	for _, table := range tables {
+
+		// create the necessary objects
+		logger := log.New()
+		scaffold := New(&table.cfg, logger)
+
+		scaffold.configurePipeline(&table.cfg.Input.Project[0])
+
+	}
 }

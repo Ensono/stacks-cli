f0561a9c421456a2f95cec3094f73c020e7589f5
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-11-18 18:49:03 +0000

    Updated documentation
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- pkg/config/framework_properties.go
+++ pkg/config/framework_properties.go
@@ -1,9 +1,9 @@
 package config
 
 type FrameworkProperties struct {
-	Prop1 string `mapstructure:"prop1"`
-	Prop2 string `mapstructure:"prop2"`
-	Prop3 string `mapstructure:"prop3"`
-	Prop4 string `mapstructure:"prop4"`
-	Prop5 string `mapstructure:"prop5"`
+	Prop1 string `mapstructure:"prop1" yaml:",omitempty"`
+	Prop2 string `mapstructure:"prop2" yaml:",omitempty"`
+	Prop3 string `mapstructure:"prop3" yaml:",omitempty"`
+	Prop4 string `mapstructure:"prop4" yaml:",omitempty"`
+	Prop5 string `mapstructure:"prop5" yaml:",omitempty"`
 }

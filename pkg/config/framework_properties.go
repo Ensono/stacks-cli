c00360eb6449ac64c00934dbc8caed9fa2b40e83
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-11-18 16:24:41 +0000

    Added properties that can be specified to allow changes in the commands that are run during scaffolding
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ pkg/config/framework_properties.go
@@ -0,0 +1,9 @@
+package config
+
+type FrameworkProperties struct {
+	Prop1 string `mapstructure:"prop1"`
+	Prop2 string `mapstructure:"prop2"`
+	Prop3 string `mapstructure:"prop3"`
+	Prop4 string `mapstructure:"prop4"`
+	Prop5 string `mapstructure:"prop5"`
+}

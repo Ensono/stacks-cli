8b0ec3056e3befe0277598782fcb144541395d8b
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-11-28 19:14:16 +0000

    Added CLI version check
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- pkg/config/options.go
+++ pkg/config/options.go
@@ -2,10 +2,11 @@ package config
 
 // Options holds the options for the CLI, such as turning on cmd logging
 type Options struct {
-	CmdLog     bool `mapstructure:"cmdlog"`
-	DryRun     bool `mapstructure:"dryrun"`
-	SaveConfig bool `mapstructure:"save" yaml:"-"`
-	NoCleanup  bool `mapstructure:"nocleanup" yaml:"-"`
-	Force      bool `mapstructure:"force" yaml:"-"`
-	NoBanner   bool `mapstructure:"nobanner"`
+	CmdLog       bool `mapstructure:"cmdlog"`
+	DryRun       bool `mapstructure:"dryrun"`
+	SaveConfig   bool `mapstructure:"save" yaml:"-"`
+	NoCleanup    bool `mapstructure:"nocleanup" yaml:"-"`
+	Force        bool `mapstructure:"force" yaml:"-"`
+	NoBanner     bool `mapstructure:"nobanner"`
+	NoCLIVersion bool `mapstructure:"nocliversion"`
 }

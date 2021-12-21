069651d132a37833c41a3265db23d12eeba2b4f8
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-12-21 09:08:33 +0000

    Removed old obsolete tasks from pipeline Updated to generate MDX files for docs so that they can be added to Docusarus
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- build/scripts/Build-Docs.ps1
+++ build/scripts/Build-Docs.ps1
@@ -25,6 +25,9 @@ param (
     $targets = @("pdf", "md")
 )
 
+# Get the script dir so that functions can be imported
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+
 # Determine the path to the project
 if ([string]::IsNullOrEmpty($BasePath)) {
     $BasePath = Get-Location
@@ -38,12 +41,22 @@ $targets | ForEach-Object -Parallel {
 
     $format = $_
 
+    . $using:scriptDir/functions/ConvertTo-MDX.ps1
+
     # Set the output directory
     $OutputDir = [IO.Path]::Combine($using:BasePath, $using:OutputDir, "docs", $format)
+
+    # Create a temporary directory to be used to store transitional files
+    $TempDir = [IO.Path]::Combine($using:BasePath, $using:OutputDir, "docs", "temp")
 
     if (!(Test-Path -Path $OutputDir)) {
         Write-Output ("Creating output dir: {0}" -f $OutputDir)
         New-Item -ItemType Directory -Path $OutputDir | Out-Null
+    }
+
+    if (!(Test-Path -Path $TempDir)) {
+        Write-Output ("Creating temporary output dir: {0}" -f $TempDir)
+        New-Item -ItemType Directory -Path $TempDir | Out-Null
     }
 
     if ($format -eq "pdf") {
@@ -76,15 +89,19 @@ $targets | ForEach-Object -Parallel {
         foreach ($file in $list) {
 
             # define the paths for the xml and md files
-            $xml_file = [IO.Path]::Combine($env:TEMP, ("{0}.xml" -f [System.IO.Path]::GetFileNameWithoutExtension($file.FullName)))
+            $xml_file = [IO.Path]::Combine($TempDir, ("{0}.xml" -f [System.IO.Path]::GetFileNameWithoutExtension($file.FullName)))
             $md_file = [IO.Path]::Combine($OutputDir, ("{0}.md" -f[System.IO.Path]::GetFileNameWithoutExtension($file.FullName)))
+            $mdx_file = [IO.Path]::Combine($using:BasePath, $using:OutputDir, "docs", "mdx", ("{0}.mdx" -f[System.IO.Path]::GetFileNameWithoutExtension($file.FullName)))
 
             # Create the cmd to run
             # -- convert to xml
             Invoke-Expression -Command ("asciidoctor -b docbook -o {0} {1}" -f $xml_file, $file.FullName)
 
             # -- convert to markdown
-            Invoke-Expression -Command ("pandoc -f docbook -t markdown_strict {0} -o {1}" -f $xml_file, $md_file)
+            Invoke-Expression -Command ("pandoc -f docbook -t gfm --wrap none {0} -o {1}" -f $xml_file, $md_file)
+
+            # -- convert to MDX format to handle Docusaurus
+            ConvertTo-MDX -Path $md_file -Destination $mdx_file
 
             # Remove the xml_file
             if (Test-Path -Path $xml_file) {

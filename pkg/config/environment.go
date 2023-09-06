f628948fe0179b9cbc7b5d339147e71bd0f8706a
balpurewal <baldip.purewal@ensono.com>
2023-09-06 16:24:10 +0100

    Added environment questions
    updated launch.json to include running stackcli in interactive mode

--- pkg/config/environment.go
+++ pkg/config/environment.go
@@ -4,7 +4,7 @@ type Environment struct {
 	Name      string `mapstructure:"name" yaml:",omitempty"`
 	StageName  string `mapstructure:"stagename" yaml:",omitempty"`
 	ShortName  string `mapstructure:"shortname" yaml:",omitempty"`
-	ProductionEquivalent bool `mapstructure:"production_equivalent" yaml:",omitempty"`
-	TriggerFromMainBranch bool `mapstructure:"trigger_from_main_branch" yaml:",omitempty"`
+	IsProduction bool `mapstructure:"isproduction" yaml:",omitempty"`
+	TriggerFromMainBranch bool `mapstructure:"triggerfrommainbranch" yaml:",omitempty"`
 	DependsOn []string `mapstructure:"depends_on" yaml:",omitempty"`
 }

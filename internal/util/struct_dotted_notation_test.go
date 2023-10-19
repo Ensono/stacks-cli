6e3aae99872810a99709d230bbaaf07a778af709
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-10-19 13:34:54 +0100

    Added function to get value of struct using dotted notation
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ internal/util/struct_dotted_notation_test.go
@@ -0,0 +1,49 @@
+package util
+
+import "testing"
+
+func TestGetFieldBYDottedPath(t *testing.T) {
+
+	// create the struct to work with
+	testStruct := struct {
+		Person struct {
+			FirstName string
+			LastName  string
+		}
+	}{
+		Person: struct {
+			FirstName string
+			LastName  string
+		}{
+			FirstName: "John",
+			LastName:  "Smith",
+		},
+	}
+
+	// create the table of tests to run
+	tables := []struct {
+		path string
+		test string
+		msg  string
+	}{
+		{
+			"Person.FirstName",
+			"John",
+			"First name should be John: %s",
+		},
+		{
+			"Person.LastName",
+			"Smith",
+			"Last name should be Smith: %s",
+		},
+	}
+
+	// iterate around the table of tests
+	for _, table := range tables {
+		value, _ := GetFieldByDottedPath(testStruct, table.path)
+
+		if value != table.test {
+			t.Errorf(table.msg, value)
+		}
+	}
+}

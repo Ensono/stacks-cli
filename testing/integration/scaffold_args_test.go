e2c7964ddcde61ad7f92ba09e51fdacc8d52e4e6
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-12-01 14:52:23 +0000

    Debugging
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- testing/integration/scaffold_args_test.go
+++ testing/integration/scaffold_args_test.go
@@ -125,10 +125,12 @@ func (suite *ArgsSuite) TestProject() {
 	// check that the project files have been namespaced with the company name properly
 	suite.T().Run("Ensure project files have been named correctly", func(t *testing.T) {
 		var firstDir string
+		var list []string
 
 		basedir := filepath.Join(suite.ProjectPath, "src", "api")
 		files, _ := os.ReadDir(basedir)
 
+		t.Logf("Reading dir: %s", basedir)
 		t.Logf("Found %d files", len(files))
 
 		// iterate around the files and get the first directory
@@ -142,17 +144,18 @@ func (suite *ArgsSuite) TestProject() {
 			t.Logf(file.Name())
 
 			if info.IsDir() {
-				firstDir = file.Name()
-				break
+				list = append(list, file.Name())
 			}
 		}
+
+		t.Logf("Files: %s", strings.Join(list, ", ")
 
 		// Check that the dirname begins with %company%
 		pattern := fmt.Sprintf("^%s.*$", suite.Company)
 		re := regexp.MustCompile(pattern)
-		matched := re.MatchString(firstDir)
+		matched := re.MatchString(list[0])
 
-		suite.Assert.Equal(true, matched, fmt.Sprintf("Project files should be namespaced with the company nam. [%s !match %s]", firstDir, pattern))
+		suite.Assert.Equal(true, matched, fmt.Sprintf("Project files should be namespaced with the company nam. [%s !match %s]", list[0], pattern))
 	})
 }
 

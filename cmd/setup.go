5d9e51ac3986d2783e45cfdf132869a0eb564587
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-12-04 22:16:10 +0000

    Removed commented out code
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- cmd/setup.go
+++ cmd/setup.go
@@ -35,13 +35,7 @@ var (
 func init() {
 
 	// declare variable that will be populated from the command line
-	// var company_name string
 	var project_name string
-	//var area string
-	//var tf_storage string
-	//var tf_group string
-	//var tf_container string
-	//var global bool
 
 	// add the sub commands for the setup command
 	setupCmd.AddCommand(setupUpdateCmd)
@@ -51,23 +45,10 @@ func init() {
 	rootCmd.AddCommand(setupCmd)
 
 	// Configure the flags for the update command
-	// setupUpdateCmd.Flags().StringVar(&company_name, "company", "", "The name of the company")
 	setupUpdateCmd.Flags().StringVar(&project_name, "project", "", "The name of the project")
-	//setupUpdateCmd.Flags().StringVar(&area, "area", "", "The area of the project")
-	//setupUpdateCmd.Flags().StringVar(&tf_storage, "storage", "", "The name of the Azure Storage account or S3 bucket being used to store the Terraform state")
-	//setupUpdateCmd.Flags().StringVar(&tf_group, "group", "", "This is the group that contains the storage account for the Terraform state.")
-	//setupUpdateCmd.Flags().StringVar(&tf_container, "container", "", "For Azure storage accounts this is the name of the container to be used for the workspace. For AWS S3 Buckets this is the name of the folder to use.")
-	//setupUpdateCmd.Flags().BoolVarP(&global, "global", "g", false, "Set the values globally. These will be set in the user's home directory")
 
 	// bind the flags to the configuration
-	// viper.BindPFlag("input.business.company", setupUpdateCmd.Flags().Lookup("company"))
 	viper.BindPFlag("input.business.project", setupUpdateCmd.Flags().Lookup("project"))
-	//viper.BindPFlag("input.business.domain", setupUpdateCmd.Flags().Lookup("area"))
-	//viper.BindPFlag("input.terraform.backend.storage", setupUpdateCmd.Flags().Lookup("storage"))
-	//viper.BindPFlag("input.terraform.backend.group", setupUpdateCmd.Flags().Lookup("group"))
-	//viper.BindPFlag("input.terraform.backend.container", setupUpdateCmd.Flags().Lookup("container"))
-	//viper.BindPFlag("input.global", setupUpdateCmd.Flags().Lookup("global"))
-
 }
 
 func executeSetupUpdate(ccmd *cobra.Command, args []string) {
